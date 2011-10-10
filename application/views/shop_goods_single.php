e6fb0aa80cfd918bf1f22eedaed5d46182ef0f23
Max <cmd@bestwebsoft.com>
2011-10-10 16:03:23 +0300

    fix shop bugs

--- application/views/shop_goods_single.php
+++ application/views/shop_goods_single.php
@@ -1,6 +1,9 @@
-	<div class="store-item">
-		<span class="history" href="javascript:void(0);"></span>
-		<div class="store-item-img"><a class="lightbox" href="<?php echo $base.$items_array['foto']?>"><img src="<?php echo $base.$items_array['thumb']?>" /></a></div>		
-		<p><?php echo $lang['price']?>:  <?php echo $items_array['price']?> грн. </p>
-		<a class="buy-btn" href="javascript:void(0);"><?php echo $lang['buy_now']?></a>
-	</div>
+<div class="store-item">		
+	<div class="store-item-img">
+		<a class="lightbox" href="<?php echo $base.$items_array['foto']?>">
+			<img src="<?php echo $base.$items_array['thumb']?>" />
+		</a>
+	</div>		
+	<p><?php echo $lang['price']?>:  <?php echo $items_array['price']?> грн. </p>
+	<a class="buy-btn" href="javascript:void(0);"><?php echo $lang['buy_now']?></a>
+</div>
--- application/views/shop_services.php
+++ application/views/shop_services.php
@@ -1,7 +1,13 @@
 <?php foreach ($items_array as $service):?>
 	<div class="store-item">
-		<div class="store-item-img"><a class="lightbox" href="<?php echo $base.$service['foto']?>"><img src="<?php echo $base.$service['thumb']?>" /></div>
-		<h3><a class="single" id="services/<?php echo $service['id']?>" href="javascript:void(0);"><?php echo $service['name'] ?></a></h3>
+		<div class="store-item-img">
+			<a class="lightbox" href="<?php echo $base.$service['foto']?>">
+				<img src="<?php echo $base.$service['thumb']?>" />
+			</a>
+		</div>
+		<h3>
+			<a class="single" id="services/<?php echo $service['id']?>" href="javascript:void(0);"><?php echo $service['name'] ?></a>
+		</h3>
 		<p><?php echo $lang['price']?>:  <?php echo $service['price']?> грн. </p>
 		<a class="buy-btn" href="javascript:void(0);"><?php echo $lang['buy_now']?></a>
 	</div>
